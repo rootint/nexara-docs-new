19c75029443a75f50048d5361b053d1764be8abd
rootint <danik.tim@gmail.com>
2026-03-30 19:59:35 +0300

    V1 working

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,33 @@
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
-import { Inter } from 'next/font/google';
+import localFont from 'next/font/local';
 
-const inter = Inter({
-  subsets: ['latin'],
+const golosText = localFont({
+  src: [
+    {
+      path: './fonts/golos-text-latin.woff2',
+      style: 'normal',
+    },
+    {
+      path: './fonts/golos-text-latin-ext.woff2',
+      style: 'normal',
+    },
+    {
+      path: './fonts/golos-text-cyrillic.woff2',
+      style: 'normal',
+    },
+    {
+      path: './fonts/golos-text-cyrillic-ext.woff2',
+      style: 'normal',
+    },
+  ],
+  display: 'swap',
+  variable: '--font-golos',
 });
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html lang="en" className={golosText.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
